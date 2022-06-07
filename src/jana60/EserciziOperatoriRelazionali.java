8b926fb0e6ebfda9006b734b0a3bc1dd3d1c4b18
Sowwtky <miche46ale@gmail.com>
2022-06-07 12:19:54 +0200

    aggiunta condizione per valore negativo etá

--- src/jana60/EserciziOperatoriRelazionali.java
+++ src/jana60/EserciziOperatoriRelazionali.java
@@ -18,15 +18,21 @@ public class EserciziOperatoriRelazionali {
 		System.out.println("Inserisci anni secondo utente:");
 		anniUtente2 = scan.nextInt();
 		
+		
 		System.out.println("Etá utente 1: " + anniUtente1);
 		System.out.println("Etá utente 2: " + anniUtente2);
 		
-		if(anniUtente1 > anniUtente2) {
-			System.out.println("Utente 1 é piú grande di utente 2");
-		} else if(anniUtente1 == anniUtente2) {
-			System.out.println("I due utenti hanno la stessa etá");
+		
+		if (anniUtente1 >= 0 && anniUtente2 >= 0) {
+			if (anniUtente1 > anniUtente2) {
+				System.out.println("Utente 1 é piú grande di utente 2");
+			} else if (anniUtente1 == anniUtente2) {
+				System.out.println("I due utenti hanno la stessa etá");
+			} else {
+				System.out.println("Utente 2 é piú grande di utente 1");
+			} 
 		} else {
-			System.out.println("Utente 2 é piú grande di utente 1");
+			System.out.println("L'etá non puó essere negativa. Riprova");
 		}
 		scan.close();
 			
