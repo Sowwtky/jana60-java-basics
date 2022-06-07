3c924aba3be40d9fe26e0dc92482bed91ccec7b0
Sowwtky <miche46ale@gmail.com>
2022-06-07 10:39:45 +0200

    password generator (input)

--- src/jana60/PasswordGenerator.java
+++ src/jana60/PasswordGenerator.java
@@ -1,8 +1,11 @@
 package jana60;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 
 	public static void main(String[] args) {
+		/*
 		//nome
 		String nomeUtente = "Alessia";
 		String cognomeUtente = "Biagi";
@@ -16,7 +19,45 @@ public class PasswordGenerator {
 		//password generator
 		String password = nomeUtente + "-" + cognomeUtente + "-" + colorePreferito + "-" + somma;
 		System.out.println("Password generata: " + password);
+		*/
 		
+		String nome;
+		String cognome;
+		String colorePreferito;
+		int giornoNascita;
+		int meseNascita;
+		int annoNascita;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Inserisci nome:");
+		nome = scan.nextLine();
+		System.out.println("Hai digitato: " + nome);
+		
+		System.out.println("Inserisci cognome:");
+		cognome = scan.nextLine();
+		System.out.println("Hai digitato: " + cognome);
+		
+		System.out.println("Inserisci colore preferito:");
+		colorePreferito = scan.nextLine();
+		System.out.println("Hai digitato: " + colorePreferito);
+		
+		System.out.println("Inserisci giorno di nascita:");
+		giornoNascita = scan.nextInt();
+		System.out.println("Hai digitato: " + giornoNascita);
+		
+		System.out.println("Inserisci mese di nascita:");
+		meseNascita = scan.nextInt();
+		System.out.println("Hai digitato: " + meseNascita);
+		
+		System.out.println("Inserisci anno di nascita:");
+		annoNascita = scan.nextInt();
+		System.out.println("Hai digitato: " + annoNascita);
+		
+		int somma = giornoNascita + meseNascita + annoNascita;
+		String password = nome + "-" + cognome + "-" + colorePreferito + "-" + somma;
+		
+		System.out.println("La tua password é: " + password);
+		scan.close();
 	}
 
 }
