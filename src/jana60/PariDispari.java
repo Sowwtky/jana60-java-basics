4bc43f98739201ff9f13d2cc3a35e33ee2a79874
Sowwtky <miche46ale@gmail.com>
2022-06-08 12:02:59 +0200

    commenti

--- src/jana60/PariDispari.java
+++ src/jana60/PariDispari.java
@@ -14,26 +14,35 @@ public class PariDispari {
 		 * se la somma é dispari e utente ha scelto dispari vince utente
 		 * altrimenti vince computer*/
 		
+		//inizializzo scanner
 		Scanner scan = new Scanner(System.in);
 		
+		//variabili
 		boolean sceltaUtentePari;
 		boolean puoiProcedere = true;
 		
+		//chiedo ad utente pari o dispari
 		System.out.println("Scegli pari o dispari:");
 		String scelta = scan.nextLine();
 		
+		//inizializzo variabile sceltaUtentePari in base alla risposta
 		if(scelta.equalsIgnoreCase("pari")) {
 			sceltaUtentePari = true;
 			
-		}else if (scelta.equalsIgnoreCase("dispari")){
+		}else if(scelta.equalsIgnoreCase("dispari")){
 			sceltaUtentePari = false;
-		}else {
+			
+		}else { //in caso di risposta non valida
 			System.out.println("Scelta non valida");
 			puoiProcedere = false;
 			sceltaUtentePari = false;
 		}
+		
+		//se scelta valida, procedo con programma
 		 if (puoiProcedere) {
 			 System.out.println("Procediamo");
+			 
+			 //chiedo ad utente numero da 0 a 5
 			 System.out.println("Tira un numero da 0 e 5");
 			 int numeroUtente = scan.nextInt();
 			 
@@ -41,18 +50,22 @@ public class PariDispari {
 			 Random randomGenerator = new Random();
 			 int numeroComputer = randomGenerator.nextInt(6);
 			 
-			 
+			 //stampo a schermo i numeri scelti
 			 System.out.println("Hai scelto: " + numeroUtente);
 			 System.out.println("Il computer ha scelto: " + numeroComputer);
 			 
+			 //faccio somma due numeri
 			 int somma = numeroUtente + numeroComputer;
 			 
+			 //se somma pari (diviso due = resto 0)
 			 if((somma % 2 ) == 0) {
 				if (sceltaUtentePari) {
 					System.out.println("Hai vinto!");
 				} else {
 					System.out.println("Hai perso!");
 				}
+				
+			 //se somma dispari
 			 }else{
 				if (!sceltaUtentePari) {
 					System.out.println("Hai vinto!");
